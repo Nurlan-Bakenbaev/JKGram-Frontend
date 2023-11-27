0698e1f2e8be25a25e88c6430ceae2542b31bebb
Nurlan <iamsamurai1991@gmail.com>
2023-11-27 13:03:19 +0100

    modified:   frontend/src/components/widgets/MyPostWidget.jsx

--- frontend/src/components/widgets/MyPostWidget.jsx
+++ frontend/src/components/widgets/MyPostWidget.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import Dropzone from "react-dropzone";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import AttachmentIcon from "@mui/icons-material/Attachment";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
@@ -7,6 +9,22 @@ import ImageIcon from "@mui/icons-material/Image";
 import MicIcon from "@mui/icons-material/Mic";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 const MyPostWidget = () => {
+  const dispatch = useDispatch();
+  const [isImage, setImage] = useState(false);
+  const [image, setImage] = useState(null);
+  const [post, setPost] = useState("");
+  const { _id } = useSelector((state = state.auth.user));
+  const token = useSelector((state) => state.auth.token);
+
+  const handlePost = async () => {
+    const formData = new FormData();
+    formData.append("userId", _id);
+    formData.append("description", post);
+    if (image) {
+      formData.append("picture", image);
+      formData.append("piturePath", image.name);
+    }
+  };
   return <div></div>;
 };
 
