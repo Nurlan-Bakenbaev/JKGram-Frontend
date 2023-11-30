53251d5456325cefb76d78052a40f67dbf8c3670
Nurlan <iamsamurai1991@gmail.com>
2023-11-30 15:35:23 +0100

    new file:   frontend/src/components/Friends.jsx 	modified:   frontend/src/components/widgets/PostWidget.jsx

---
+++ frontend/src/components/Friends.jsx
@@ -0,0 +1,11 @@
+import {
+  PersonAddAlt1Outlined,
+  PersonRemoveAlt1Outlined,
+} from "@mui/icons-material";
+const Friends = () => {
+  return <div>
+    
+  </div>;
+};
+
+export default Friends;
--- frontend/src/components/widgets/PostWidget.jsx
+++ frontend/src/components/widgets/PostWidget.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-
+import React from "react";
+import {
+  ChatBubbleOutlineIcon,
+  FavoriteBorderOutlined,
+  FavoriteOutlined,
+  ShareOutlined,
+} from "@mui/icons-material";
+import Friends from "../Friends";
 const PostWidget = () => {
-  return (
-    <div>PostWidget</div>
-  )
-}
+  return <div>PostWidget</div>;
+};
 
-export default PostWidget
+export default PostWidget;
