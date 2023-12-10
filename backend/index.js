df2c3b75bc16feaa3cc946876a234db2982b7d7d
Nurlan <iamsamurai1991@gmail.com>
2023-12-10 14:15:57 +0100

    modified:   backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://glistening-liger-94a733.netlify.app/"
+    "https://glistening-liger-94a733.netlify.app"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
