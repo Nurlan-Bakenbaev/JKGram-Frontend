3a7c44b65ff43010e98fd354e969ca3fbe1a861a
Nurlan <iamsamurai1991@gmail.com>
2023-12-10 14:28:52 +0100

    modified:   backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -29,15 +29,23 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "50mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
+  const allowedOrigins = [
     "https://glistening-liger-94a733.netlify.app",
-    "http://localhost:5173"
-  );
+    "http://localhost:5173",
+  ];
+
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
+
   next();
 });
+
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 //FILE STORAGE
